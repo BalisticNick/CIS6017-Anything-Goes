2a67b59da5bf29851e3b25f08e65d238f581885a
Shane <shanesealey@hotmail.com>
2023-03-17 16:20:27 +0000

    Working on Colliders

---
+++ MobileGameProject/Assets/Scripts/ButtonController.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonController : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        
+    }
+}
